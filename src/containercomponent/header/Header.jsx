2e3e4a2c91f9d968b092aecf18d8c0c3f818e94f
Sonu Mondal <takeinfo2u@gmail.com>
2023-10-01 16:27:41 +0530

    Resolve some warning of vercel to deploy without any issue

--- src/containercomponent/header/Header.jsx
+++ src/containercomponent/header/Header.jsx
@@ -1,5 +1,5 @@
 import Zomato from "../../component/zomato/Zomato"
-import Search from "../../component/Search/Search"
+import Search from "../../component/search/Search"
 import LoginStatus from "../../component/loginstatus/LoginStatus"
 import "./Header.css"
 
