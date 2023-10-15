8655362f12cfff597907f78490340edc3ef0c129
Sonu Mondal <takeinfo2u@gmail.com>
2023-10-15 22:54:45 +0530

    resolve bug related searching restaurant

--- src/component/search/Search.jsx
+++ src/component/search/Search.jsx
@@ -1,12 +1,20 @@
 import { BsSearch } from "react-icons/bs"
 import "./Search.css"
 import location from "../../assets/zomato/location.jpg"
-import {useState} from "react"
+// import {useState } from "react"
 
 import { RxCross1 } from "react-icons/rx"
 
 function Search(props) {
-    const [openSerach, setOpenSearch] = useState(false)
+    // const [openSerach, setOpenSearch] = useState(false)
+
+    // useEffect(()=>{
+    //     console.log("useeffect is running");
+    //     if(props.search==""){
+    //         setOpenSearch(false)
+    //         console.log("useeffect showed action");
+    //     }
+    // })
 
     return (
         <div id="search-box-main">
@@ -15,17 +23,23 @@ function Search(props) {
                 <div style={{ color: "grey" }}>Kolkata</div>
             </div>
             <div id="search-item">
-                <BsSearch onClick={()=>setOpenSearch(true)} style={{
+                <BsSearch onClick={props.setopensearch} style={{
                     color: "grey", height: "1.5rem",
                     width: "1.5rem"
                 }} />
                 {
-                    openSerach ?
+                    props.opensearch ?
                         <>
                             <div className="open-search">
+                            <RxCross1 className="cross-icon-open-search" onClick={props.deleteopensearch} />
                                 <div className="open-search-child">
-                                    <input onChange={props.search} type="search" placeholder="Search for restaurant, cuisine or a dish" />
-                                    <RxCross1 onClick={()=>setOpenSearch(false)}/>
+                                    <div className="open-search-grand-child">
+                                        <BsSearch style={{
+                                            color: "grey", height: "1rem",
+                                            width: "1rem"
+                                        }} />
+                                        <input onChange={props.search} type="search" placeholder="Search for restaurant, cuisine or a dish" />
+                                    </div>
                                 </div>
                             </div>
                         </> :
--- src/containercomponent/header/Header.jsx
+++ src/containercomponent/header/Header.jsx
@@ -2,16 +2,26 @@ import Zomato from "../../component/zomato/Zomato"
 import Search from "../../component/search/Search"
 import LoginStatus from "../../component/loginstatus/LoginStatus"
 import "./Header.css"
+import { useState } from "react"
 
 function Header(props) {
 
     console.log("status in header", props.status);
     const defaultValue = 0
 
+    const [openSerach, setOpenSearch] = useState(false)
+
+    function setopensearch(){
+        setOpenSearch(true)
+    }
+    function deleteopensearch(){
+        setOpenSearch(false)
+    }
+
     return (
         <div id="header">
-            <Zomato status={props.status || defaultValue} />
-            <Search search={props.search}/>
+           <Zomato status={props.status || defaultValue} />
+            <Search opensearch={openSerach} setopensearch={setopensearch} deleteopensearch={deleteopensearch} search={props.search}/>
             <LoginStatus status={props.status || defaultValue} />
         </div>
     )
