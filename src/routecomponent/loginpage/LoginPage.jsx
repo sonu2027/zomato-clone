895a7695d65fe697edafdb90affacab54330aff3
Sonu Mondal <takeinfo2u@gmail.com>
2024-05-08 22:42:39 +0530

    edited login page

--- src/routecomponent/loginpage/LoginPage.jsx
+++ src/routecomponent/loginpage/LoginPage.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./LoginPage.css"
 import { RxCross1 } from "react-icons/rx"
 import { useNavigate } from "react-router-dom"
+import { MdEmail } from "react-icons/md";
 
 function LoginPage() {
 
@@ -26,6 +27,14 @@ function LoginPage() {
         }
     }
 
+    useEffect(() => {
+        setTimeout(function () {
+            document.getElementById("login-form-main-div").style.opacity = "1"; // Show the box after 0 seconds
+            document.getElementById("login-form-main-div").style.transform = "translateY(0)"; // Move the box from bottom to top
+        });
+    }, [])
+
+
     return (
         <div id="login-form-main-div">
             <div id="login-form">
@@ -35,16 +44,33 @@ function LoginPage() {
                         <RxCross1 />
                     </Link>
                 </div>
-                <div id="input-tag">
+                <form id="input-tag">
                     <input value={email} onChange={(e) => setEmail(e.target.value)}
                         type="text" placeholder="Enter username as admin" />
                     <input value={password} onChange={(e) => setPassword(e.target.value)}
                         type="password" name="" id="" placeholder="Enter password as password" />
+                    <button onClick={authenticateUser}>Log in</button>
+                </form>
+                <div className="line">
+                    <hr />
+                    <div> or </div>
+                    <hr />
+                </div>
+                <div className="email">
+                    <MdEmail className="email-icon" />
+                    <div>Continue with email</div>
+                </div>
+                <div className="line">
+                    <div>New to zomato ? </div>
+                    <div className="create">Create account</div>
+                </div>
+                <div className="line">
+                    <div>Do you have restaurant?</div>
+                    <Link to="/partner/register"><div className="create">Register here</div></Link>
                 </div>
                 {
                     showStatus.length > 0 && <div>{showStatus}</div>
                 }
-                <button onClick={authenticateUser}>Log in</button>
             </div>
         </div>
     )
