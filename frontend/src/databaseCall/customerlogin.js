d6c3fd8545f3000be52fc9dfe7cf7561370b7b9d
Sonu Mondal <takeinfo2u@gmail.com>
2024-07-23 22:27:17 +0530

    added cors origin

--- frontend/src/databaseCall/customerlogin.js
+++ frontend/src/databaseCall/customerlogin.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const loginCustomer = async (email, password) => {
   try {
+    //added cors origin
     const response = await axios.post(
       `${import.meta.env.VITE_API_URL}/user/login`,
       JSON.stringify({ email, password }),
