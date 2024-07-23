adf1c77dad1509ed3ac2f7e1dfc77a3fe3f2f406
Sonu Mondal <takeinfo2u@gmail.com>
2024-07-23 15:34:48 +0530

    moved index.js from backend src to root

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
-import connectDB from "./connectDB/connectDB.js";
+import app from "./src/app.js";
+import connectDB from "./src/connectDB/connectDB.js";
 import dotenv from "dotenv"
 
 dotenv.config({
