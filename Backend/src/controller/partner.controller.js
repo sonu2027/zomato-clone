ac8b487c999f81c93d314ff262d4ec1c10cdd4aa
Sonu Mondal <takeinfo2u@gmail.com>
2024-08-29 07:18:27 +0530

    changed pass for sending email with nodemailer

--- Backend/src/controller/partner.controller.js
+++ Backend/src/controller/partner.controller.js
@@ -28,7 +28,7 @@ const sendEmail = async (req, res) => {
     from: "sonu.mondal.2027@gmail.com",
     to: email,
     subject: "Email Verification Code",
-    text: `Your verification code is: ${otp}. Enter this verification to proceed`,
+    text: `Your verification code is: ${otp}`,
   };
 
   console.log("mailOptions: ", mailOptions);
